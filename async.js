ac2c8fc434a257770d04401052388d7345bb97cc
qwakyyy <malditojmin@gmail.com>
2025-04-05 20:39:18 +0900

    added async.js

---
+++ async.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const util = require('util');
+const readFile = util.promisify(fs.readFile);
+const printFileSize = asyncfilename => {
+    try {
+        constdata= await readFile(filename);
+        console.log(`Datalength: ${data.length} bytes`);
+        } catch (err) {
+            console.error(err);
+        }
+};
+
+console.log('String1');
+printFileSize('./file.bin');
+console.log('String2');
